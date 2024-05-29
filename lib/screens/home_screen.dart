c5d23ce9f84cd51fd5ac56ef91f228ea5f94ed95
Ru2Lu <jiutianlian0@gmail.com>
2024-05-29 22:20:09 +0900

    今月の予算を入力するダイアログを表示

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int? _monthlyBudget;
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        _showMonthlyBudgetInputDialog();
+      },
+    );
+  }
+
+  Future<void> _showMonthlyBudgetInputDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            '今月使用できる金額を入力してください',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          content: TextFormField(
+            controller: _controller,
+            autofocus: true,
+            keyboardType: TextInputType.number,
+            style: const TextStyle(fontSize: 42),
+            decoration: const InputDecoration(
+              hintText: '300,000',
+              suffixText: '円',
+            ),
+            onChanged: (budgetValue) {
+              budgetValue = _formMoneyString(
+                budgetValue.replaceAll(',', ''),
+              );
+              _controller.value = TextEditingValue(
+                text: budgetValue,
+                selection: TextSelection.collapsed(
+                  offset: budgetValue.length,
+                ),
+              );
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _monthlyBudget = int.tryParse(
+                    _controller.text.replaceAll(',', ''),
+                  );
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('確定'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String _formMoneyString(String money) {
+    return NumberFormat.decimalPattern().format(
+      int.parse(money),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +87,11 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('ホーム画面'),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          'ホーム画面',
+          _monthlyBudget != null
+              ? '今月使用できる金額は${_formMoneyString(_monthlyBudget.toString())}円です'
+              : '今月使用できる金額は-円です',
         ),
       ),
     );
