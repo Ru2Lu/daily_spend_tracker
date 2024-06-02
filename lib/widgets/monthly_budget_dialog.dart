f9e6107cf1b1b291e2d74fdbaaaa5c6c62faad3f
Ru2Lu <jiutianlian0@gmail.com>
2024-06-02 21:56:02 +0900

    ダイアログをConsumerWidgetに修正

--- lib/widgets/monthly_budget_dialog.dart
+++ lib/widgets/monthly_budget_dialog.dart
@@ -5,84 +5,71 @@ import '../providers/dialog/dialog_controller_provider.dart';
 import '../providers/dialog/dialog_error_message_provider.dart';
 import '../utils/format.dart';
 
-class MonthlyBudgetDialog extends ConsumerStatefulWidget {
+class MonthlyBudgetDialog extends ConsumerWidget {
   const MonthlyBudgetDialog({super.key});
 
   @override
-  MonthlyBudgetDialogState createState() => MonthlyBudgetDialogState();
-}
-
-class MonthlyBudgetDialogState extends ConsumerState<MonthlyBudgetDialog> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.watch(dialogControllerProvider);
-    return StatefulBuilder(builder: (
-      BuildContext context,
-      StateSetter setState,
-    ) {
-      return AlertDialog(
-        title: const Text(
-          '今月使用できる金額を入力してください',
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-          ),
+    final errorMessage = ref.watch(dialogErrorMessageProvider);
+
+    return AlertDialog(
+      title: const Text(
+        '今月使用できる金額を入力してください',
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
         ),
-        content: Consumer(
-          builder: (context, ref, _) {
-            final errorMessage = ref.watch(dialogErrorMessageProvider);
-            return TextFormField(
-              controller: controller,
-              autofocus: true,
-              keyboardType: TextInputType.number,
-              style: const TextStyle(fontSize: 42),
-              decoration: InputDecoration(
-                hintText: '300,000',
-                suffixText: '円',
-                errorText: errorMessage,
-              ),
-              onChanged: (budgetValue) {
-                if (budgetValue.isNotEmpty) {
-                  ref
-                      .read(dialogErrorMessageProvider.notifier)
-                      .clearDialogErrorMessage();
-                }
-                budgetValue = formatCommaSeparateNumber(
-                  budgetValue.replaceAll(',', ''),
-                );
-                controller.value = TextEditingValue(
-                  text: budgetValue,
-                  selection: TextSelection.collapsed(
-                    offset: budgetValue.length,
-                  ),
-                );
-              },
-            );
+      ),
+      content: TextFormField(
+        controller: controller,
+        autofocus: true,
+        keyboardType: TextInputType.number,
+        style: const TextStyle(fontSize: 42),
+        decoration: InputDecoration(
+          hintText: '300,000',
+          suffixText: '円',
+          errorText: errorMessage,
+        ),
+        onChanged: (budgetValue) {
+          if (budgetValue.isNotEmpty) {
+            ref
+                .read(dialogErrorMessageProvider.notifier)
+                .clearDialogErrorMessage();
+          }
+          budgetValue = formatCommaSeparateNumber(
+            budgetValue.replaceAll(',', ''),
+          );
+          controller.value = TextEditingValue(
+            text: budgetValue,
+            selection: TextSelection.collapsed(
+              offset: budgetValue.length,
+            ),
+          );
+        },
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            if (controller.text.isEmpty) {
+              // 金額が入力されていない場合は確定を押してもダイアログが閉じない
+              ref
+                  .read(dialogErrorMessageProvider.notifier)
+                  .setDialogErrorMessage('金額を入力してください');
+            } else {
+              // 金額が入力されてる場合はダイアログを閉じる
+              final monthlyBudget = int.tryParse(
+                controller.text.replaceAll(',', ''),
+              );
+              ref
+                  .read(monthlyBudgetProvider.notifier)
+                  .setMonthlyBudget(monthlyBudget);
+              Navigator.of(context).pop();
+            }
           },
+          child: const Text('確定'),
         ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              if (controller.text.isEmpty) {
-                // 金額が入力されていない場合は確定を押してもダイアログが閉じない
-                ref
-                    .read(dialogErrorMessageProvider.notifier)
-                    .setDialogErrorMessage('金額を入力してください');
-              } else {
-                // 金額が入力されてる場合はダイアログを閉じる
-                final monthlyBudget = int.tryParse(
-                  controller.text.replaceAll(',', ''),
-                );
-                ref
-                    .read(monthlyBudgetProvider.notifier)
-                    .setMonthlyBudget(monthlyBudget);
-                Navigator.of(context).pop();
-              }
-            },
-            child: const Text('確定'),
-          ),
-        ],
-      );
-    });
+      ],
+    );
   }
 }
