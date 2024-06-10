9bdfb1da0b579a2da61fd61bba19f9f74a28e9b3
Ru2Lu <jiutianlian0@gmail.com>
2024-06-11 08:57:12 +0900

    フォーマットの引数に空文字が与えられた時に発生するエラーの修正

--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -2,6 +2,9 @@ import 'package:intl/intl.dart';
 
 /// 数字の文字列を3桁ごとにカンマ区切りに整形
 String formatCommaSeparateNumber(String numString) {
+  if (numString == "") {
+    return "";
+  }
   return NumberFormat.decimalPattern().format(
     int.parse(numString),
   );
