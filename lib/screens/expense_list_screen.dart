6933345a78788ac2a019cd47abf74864aea1bc94
Ru2Lu <jiutianlian0@gmail.com>
2024-06-12 12:58:59 +0900

    1日当たりに使用出来る金額を表示

--- lib/screens/expense_list_screen.dart
+++ lib/screens/expense_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:daily_spend_tracker/providers/monthly_budget_service_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../utils/format.dart';
 
 class ExpenseListScreen extends ConsumerWidget {
   const ExpenseListScreen({
@@ -8,13 +10,25 @@ class ExpenseListScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final monthlyBudget = ref.watch(monthlyBudgetProvider).value?.budget;
+
+    final now = DateTime.now();
+    // 今月の日数
+    final daysInThisMonth = DateUtils.getDaysInMonth(
+      now.year,
+      now.month,
+    );
+    // 今日使用出来る金額
+    final dayBudget = ((monthlyBudget ?? 0) / daysInThisMonth).floor();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('今月の支出画面'),
       ),
-      body: const Center(
-        child: Text('今月の支出!'),
+      body: Center(
+        child: Text('今日使用出来る金額は${formatCommaSeparateNumber(
+          dayBudget.toString(),
+        )}円です'),
       ),
     );
   }
