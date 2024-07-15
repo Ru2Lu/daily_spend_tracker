af6c3e0de1b0f13e6c947e137175441419739f72
Ru2Lu <jiutianlian0@gmail.com>
2024-07-15 22:04:19 +0900

    支出項目を左にスライドすると編集ボタンと削除ボタンが見えるよう修正

--- lib/widgets/expense/expense_card.dart
+++ lib/widgets/expense/expense_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../utils/format.dart';
 
 class ExpenseCard extends StatelessWidget {
@@ -16,37 +17,59 @@ class ExpenseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // 支出のタイトル
-            Expanded(
-              flex: 2,
-              child: Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  overflow: TextOverflow.ellipsis,
+    return Slidable(
+      key: const ValueKey(0),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (BuildContext context) {},
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            label: '編集',
+          ),
+          SlidableAction(
+            onPressed: (BuildContext context) {},
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: '削除',
+          ),
+        ],
+      ),
+      child: Card(
+        child: ListTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // 支出のタイトル
+              Expanded(
+                flex: 2,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
               ),
-            ),
-            // 支出の金額
-            Expanded(
-              flex: 1,
-              child: Text(
-                '${formatCommaSeparateNumber(amount)}円',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  overflow: TextOverflow.ellipsis,
+              // 支出の金額
+              Expanded(
+                flex: 1,
+                child: Text(
+                  '${formatCommaSeparateNumber(amount)}円',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  textAlign: TextAlign.end,
                 ),
-                textAlign: TextAlign.end,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
