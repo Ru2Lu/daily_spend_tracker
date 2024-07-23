dcf42b082f2a2df63dfc56b6ff8479661af72dc4
Ru2Lu <jiutianlian0@gmail.com>
2024-07-23 23:56:23 +0900

    本日までの収支を表示

--- lib/screens/expense_list_screen.dart
+++ lib/screens/expense_list_screen.dart
@@ -25,6 +25,15 @@ class ExpenseListScreen extends ConsumerWidget {
     );
     // 今日使用出来る金額
     final dayBudget = ((monthlyBudget ?? 0) / daysInThisMonth).floor();
+    // 今日までの今月の日数
+    final daysInThisMonthUntilToday = DateTimeRange(
+          start: DateTime(now.year, now.month, 1),
+          end: now,
+        ).duration.inDays +
+        1;
+    // 今日までの予算
+    final budgetUntilToday = dayBudget * daysInThisMonthUntilToday;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -37,18 +46,42 @@ class ExpenseListScreen extends ConsumerWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          // 今日使用出来る金額
-          Text('今日使用出来る金額は${formatCommaSeparateNumber(
-            dayBudget.toString(),
-          )}円です'),
-          // 支出一覧リスト
-          ExpenseList(
-            expensesAsyncValue: expensesAsyncValue,
-            dayBudget: dayBudget,
-          ),
-        ],
+      body: expensesAsyncValue.when(
+        data: (expenses) {
+          // 今日までの支出金額を計算
+          final expenseAmountsUntilToday = (expenses ?? [])
+              .where((expense) =>
+                  expense.date != null &&
+                  expense.date!.year == now.year &&
+                  expense.date!.month == now.month &&
+                  expense.date!.isBefore(
+                    now.add(
+                      const Duration(days: 1),
+                    ),
+                  ))
+              .fold(0, (prev, curr) => prev + (curr.amount ?? 0));
+
+          // 今日までの収支
+          final thisMonthBalance = budgetUntilToday - expenseAmountsUntilToday;
+
+          return Column(
+            children: [
+              // 本日までの収支
+              ThisMonthBalance(thisMonthBalance: thisMonthBalance),
+              // 支出一覧リスト
+              ExpenseList(
+                expensesAsyncValue: AsyncData(expenses),
+                dayBudget: dayBudget,
+              ),
+            ],
+          );
+        },
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (err, stack) => const Center(
+          child: Text('エラーが発生しました'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -69,5 +102,58 @@ class ExpenseListScreen extends ConsumerWidget {
   String _formatFullDateWithWeekday(DateTime date) {
     final DateFormat formatter = DateFormat('yyyy/M/d(E)', 'ja');
     return formatter.format(date);
+  }
+}
+
+/// 本日までの収支
+class ThisMonthBalance extends StatelessWidget {
+  const ThisMonthBalance({
+    required this.thisMonthBalance,
+    super.key,
+  });
+
+  final int thisMonthBalance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              '本日までの収支',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Icon(
+                  thisMonthBalance > 0
+                      ? Icons.trending_up
+                      : Icons.trending_down,
+                  color: thisMonthBalance > 0 ? Colors.green : Colors.red,
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  '${formatCommaSeparateNumber(
+                    thisMonthBalance.toString(),
+                  )}円',
+                  style: TextStyle(
+                    color: thisMonthBalance > 0 ? Colors.green : Colors.red,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
