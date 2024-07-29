5f4b8084bc0a0b542e47ef99f46d3e08556f6089
Ru2Lu <jiutianlian0@gmail.com>
2024-07-29 22:59:00 +0900

    履歴画面で支出一覧を表示

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,16 +1,52 @@
+import 'package:daily_spend_tracker/providers/expense/expense_service_provider.dart';
+import 'package:daily_spend_tracker/providers/monthly_budget_service_provider.dart';
+import 'package:daily_spend_tracker/widgets/expense/expense_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends ConsumerWidget {
   const HistoryScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final monthlyBudget = ref.watch(monthlyBudgetProvider).value?.budget;
+    final expensesAsyncValue = ref.watch(expensesProvider);
+
+    final now = DateTime.now();
+    // 今月の日数
+    final daysInThisMonth = DateUtils.getDaysInMonth(
+      now.year,
+      now.month,
+    );
+    // 今日使用出来る金額
+    final dayBudget = ((monthlyBudget ?? 0) / daysInThisMonth).floor();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('履歴画面！！！'),
+      body: expensesAsyncValue.when(
+        data: (expenses) {
+          return (expenses ?? []).isEmpty
+              ? const Center(
+                  child: Text('履歴がありません'),
+                )
+              : Column(
+                  children: [
+                    // 支出一覧リスト
+                    ExpenseList(
+                      expensesAsyncValue: AsyncData(expenses),
+                      dayBudget: dayBudget,
+                    ),
+                  ],
+                );
+        },
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (err, stack) => const Center(
+          child: Text('エラーが発生しました'),
+        ),
       ),
     );
   }
