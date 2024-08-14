59d6134c0218f3a3f9988f05c0ba6509e0129931
Ru2Lu <jiutianlian0@gmail.com>
2024-08-14 20:23:33 +0900

    プライバシーポリシーサイトへの導線を追加

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -9,19 +10,24 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Column(
+      body: Column(
         children: [
           ListTile(
-            title: Text(
+            title: const Text(
               'プライバシーポリシー',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
               ),
             ),
-            leading: Icon(Icons.shield),
-            trailing: Icon(Icons.arrow_forward_ios),
+            leading: const Icon(Icons.shield),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () async {
+              final url = Uri.parse(
+                  'https://ru2lu.github.io/daily_spend_tracker_privacy_policy_site/');
+              await launchUrl(url);
+            },
           ),
-          ListTile(
+          const ListTile(
             title: Text(
               '全てのデータを削除する',
               style: TextStyle(
